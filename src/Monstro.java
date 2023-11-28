486d60e3649883f1eeec48fe918e4055cc9a0e83
natan <natanrodrigo$@gmail.com>
2023-11-28 16:46:00 -0300

    viloes 2023

---
+++ src/Monstro.java
@@ -0,0 +1,33 @@
+public class Monstro {
+    public  Monstro(String nome, int pontoDeVida , int ataqueFraco, int ataqueForte,int especial, int velocidade){
+        Monstro aberracaoSombria = new Monstro("Aberração sombria", 100, 36,90,100,6);
+        Monstro anciaoDosMares = new Monstro("Ancião dos mares", 100, 46,70,9,69);
+        Monstro banshee = new Monstro("Banshee", 1,1,1,1,1);
+        Monstro cavaleiroDaMontanha = new Monstro("Cavaleiro da montanha", 1,1,1,1,1);
+        Monstro crawler = new Monstro("Crawler",1,1,1,1,1);
+        Monstro devoradorDeAlma = new Monstro("Devorador de alma",1,1,1,1,1);
+        Monstro devoradorDeMentes = new Monstro("Devorador de mentes",1,1,1,1,1);
+        Monstro dracoDoAbismo = new Monstro("Draco do abismo",1,1,1,1,1);
+        Monstro duergar = new Monstro("Duergar",1,1,1,1,1);
+        Monstro espectro = new Monstro("Espectro",1,1,1,1,1);
+        Monstro espiritoArtomentado = new Monstro("Espirito artomentado", 1,1,1,1,1);
+        Monstro fadaNegra = new Monstro("Fada negra", 1,1,1,1,1);
+        Monstro giganteDaMontanha = new Monstro("Gigante da montanha",1,1,1,1,1);
+        Monstro golemDePedra = new Monstro("Golem de pedra",1,1,1,1,1);
+        Monstro gorgon = new Monstro("Gorgon",1,1,1,1,1);
+        Monstro guardiaoAquatico = new Monstro("Guardião aquático",1,1,1,1,1);
+        Monstro guardiaoDasProfundezas = new Monstro("Guardião das produndezas",1,1,1,1,1);
+        Monstro hidra = new Monstro("Hídra",1,1,1,1,1);
+        Monstro leviata = new Monstro("Leviatã",1,1,1,1,1);
+        Monstro lich = new Monstro("Lich",1,1,1,1,1);
+        Monstro lordeDasSombra = new Monstro("Lorde Das Sombra",1,1,1,1,1);
+        Monstro metamorfoDaCaverna = new Monstro("Metamorfo da carvena",1,1,1,1,1);
+        Monstro murloc = new Monstro("Murloc",1,1,1,1,1);
+        Monstro nagaDasTrevas = new Monstro("Naga das trevas",1,1,1,1,1);
+        Monstro necromante = new Monstro("Necromante",1,1,1,1,1);
+        Monstro sirena = new Monstro("Sirena",1,1,1,1,1);
+        Monstro tritao = new Monstro("Tritão",1,1,1,1,1);
+        Monstro troglodita = new Monstro("Troglodita",1,1,1,1,1);
+        Monstro trollDasCavernas = new Monstro("Troll das carvenas",1,1,1,1,1);
+    }
+}
